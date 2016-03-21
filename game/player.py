cfdf5b8e8268cdbcb04d07ffd433bfc3741894a0
kittie <karis4@pdx.edu>
2016-03-21 00:33:40 -0700

    finished adding a ship methods

--- game/player.py
+++ game/player.py
@@ -3,6 +3,7 @@ from ship import Ship
 
 
 class Player(object):
+    ships = []
 
     def __init__(self, board_size, **kwargs):
         self.board = Board(board_size)
@@ -11,6 +12,16 @@ class Player(object):
             setattr(self, key, value)
 
     def place_ships(self, size, location, direction, name):
-
-
-
+        if direction == 'n':
+            locations = [(location[0], y) for y in range(location[1], location[1]+size)]
+        elif direction == 's':
+            locations = [(location[0], location[1]-y) for y in range(size)]
+        elif direction == 'e':
+            locations = [(x, location[1]) for x in range(location[0], location[0]+size)]
+        elif direction == 'w':
+            locations = [(location[0]-x, location[1]) for x in range(size)]
+        if self.board.place_ship(locations):
+            self.ships.append(Ship(name, locations))
+            return True
+        else:
+            return False
